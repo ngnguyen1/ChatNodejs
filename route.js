1b363bee55d940ef52b6d0c8c4ad501a4e56a49c
NgaNguyenDuy <kyo1508@gmail.com>
2014-06-13 07:58:01 +0700

    update router

--- route.js
+++ route.js
@@ -8,4 +8,8 @@ module.exports = function(router) {
     router.get('/chat', function(req, res) {
         res.render('chat.ejs');
     });
+    
+    router.post('/images/uploads', function(req, res) {
+        console.log(JSON.stringify(req));
+    });
 };
